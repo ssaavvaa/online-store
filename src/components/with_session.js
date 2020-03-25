af70a82059e8c078ce4ad858e5056dcfce604a8f
ssaavvaa <Toxasava@icloud.com>
2020-03-25 02:39:06 -0400

    aweaweawrwar

--- src/components/with_session.js
+++ src/components/with_session.js
@@ -2,15 +2,35 @@ import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_CURRENT_USER, GET_CATEGORIES } from "../graphql/queries";
 
+function foo() {
+  return console.log('foo')
+}
+
 export default Component => props => {
   const { loading, data, error, client } = useQuery(GET_CURRENT_USER);
   const categoriesData = useQuery(GET_CATEGORIES);
   if (error) console.log(error)
   if (error) return <p>Error...</p>
-  if (categoriesData.loading || loading) return <p
-    style={{ textAlign: 'center', marginTop: '50px', fontSize: '40px', fontWeight: 'bold' }}>
-    Loading...
-    </p>
+  if (categoriesData.loading || loading) return <div style={{
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: '100vw',
+    height: '100vh',
+    background: 'white',
+    zIndex: 99
+  }}>
+    <>
+
+      <Component
+        categories={[]}
+        location={{ state: null }}
+        getCurrentUser={null}
+        resetStore={foo()}
+      />
+    </>
+  </div>
+
 
 
   const { getCurrentUser } = data;
